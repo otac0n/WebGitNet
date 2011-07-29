f507976cded83b063e1f6e87d2fdc94087758699
John Gietzen <otac0n@gietzen.us>
2011-07-29 10:52:14 -0500

    Added a `GitUtilities` class for executing Git commands.

---
+++ WebGitNet/GitUtilities.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GitUtilities.cs" company="(none)">
+//  Copyright © 2011 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+//-----------------------------------------------------------------------
+
+namespace WebGitNet
+{
+    using System.Diagnostics;
+    using System.Web.Configuration;
+
+    public static class GitUtilities
+    {
+        public static string Execute(string command, string workingDir)
+        {
+            var git = WebConfigurationManager.AppSettings["GitCommand"];
+            var startInfo = new ProcessStartInfo(git, command)
+            {
+                WorkingDirectory = workingDir,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            using (var process = Process.Start(startInfo))
+            {
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+
+        public static void UpdateServerInfo(string repoPath)
+        {
+            Execute("update-server-info", repoPath);
+        }
+    }
+}
