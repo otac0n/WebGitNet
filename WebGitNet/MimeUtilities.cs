02fcf1da5636e2c8c43342be2de4ba05b26fb2bb
John Gietzen <otac0n@gietzen.us>
2011-08-16 22:34:36 -0500

    Added mappings from mime-types to SHJS languages.

--- WebGitNet/MimeUtilities.cs
+++ WebGitNet/MimeUtilities.cs
@@ -10,13 +10,18 @@ namespace WebGitNet
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Reflection;
+    using System.Text.RegularExpressions;
     using Microsoft.Win32;
 
     public class MimeUtilities
     {
         private static readonly Dictionary<string, string> knownExtensions = new Dictionary<string, string>();
         private static readonly Dictionary<string, string> knownNames = new Dictionary<string, string>();
+
+        private static readonly List<KeyValuePair<string, string>> exactLangMatches = new List<KeyValuePair<string, string>>();
+        private static readonly List<KeyValuePair<string, Regex>> regexLangMatches = new List<KeyValuePair<string, Regex>>();
 
         static MimeUtilities()
         {
@@ -42,6 +47,44 @@ namespace WebGitNet
                     else
                     {
                         knownNames[file] = type;
+                    }
+                }
+            }
+            catch (IOException)
+            {
+            }
+
+            try
+            {
+                foreach (var line in File.ReadAllLines(Path.Combine(assemblyPath, "shjs-languages.txt")))
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    var index = line.IndexOfAny(new[] { '=', '%', '@' });
+                    var lang = line.Substring(0, index);
+                    var rest = line.Substring(index + 1);
+
+                    var op = line[index];
+
+                    if (op == '=')
+                    {
+                        exactLangMatches.Add(new KeyValuePair<string, string>(lang, rest));
+                    }
+                    else if (op == '@')
+                    {
+                        exactLangMatches.Add(new KeyValuePair<string, string>(lang, "text/x-" + rest));
+                        exactLangMatches.Add(new KeyValuePair<string, string>(lang, "text/x-" + rest + "-source"));
+                        exactLangMatches.Add(new KeyValuePair<string, string>(lang, "text/x-" + rest + "-src"));
+                        exactLangMatches.Add(new KeyValuePair<string, string>(lang, "application/x-" + rest));
+                        exactLangMatches.Add(new KeyValuePair<string, string>(lang, "application/x-" + rest + "-source"));
+                        exactLangMatches.Add(new KeyValuePair<string, string>(lang, "application/x-" + rest + "-src"));
+                    }
+                    else if (op == '%')
+                    {
+                        regexLangMatches.Add(new KeyValuePair<string, Regex>(lang, new Regex(rest)));
                     }
                 }
             }
@@ -92,6 +135,24 @@ namespace WebGitNet
             }
 
             return "application/octet-stream";
+        }
+
+        public static string GetLanguage(string mimeType)
+        {
+            var lang = (from m in exactLangMatches
+                        where m.Value == mimeType
+                        select m.Key).FirstOrDefault();
+
+            if (lang != null)
+            {
+                return lang;
+            }
+
+            lang = (from m in regexLangMatches
+                    where m.Value.IsMatch(mimeType)
+                    select m.Key).FirstOrDefault();
+
+            return lang;
         }
     }
 }
