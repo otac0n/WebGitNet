260dc602f6197f59331218db579b612cdd7ab791
John Gietzen <otac0n@gietzen.us>
2011-09-18 17:05:58 -0500

    Added a `WebGitNet.RepoImpact` assembly to hold the "View Repo Impact" controller.

---
+++ WebGitNet.RepoImpact/AssemblyInstaller.cs
@@ -0,0 +1,17 @@
+﻿namespace WebGitNet.RepoImpact
+{
+    using System.Web.Mvc;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+
+    public class AssemblyInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(AllTypes.FromThisAssembly()
+                                       .BasedOn<IController>()
+                                       .Configure(c => c.LifeStyle.Transient));
+        }
+    }
+}
