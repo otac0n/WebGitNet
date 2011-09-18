8a58ee53da7975fbd2e6928e035ac07d0cbf42d2
John Gietzen <otac0n@gietzen.us>
2011-09-18 12:49:38 -0500

    Added a shared library project.

--- WebGitNet/Properties/AssemblyInfo.cs
+++ WebGitNet/Properties/AssemblyInfo.cs
@@ -3,17 +3,4 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WebGit .NET")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("John Gietzen")]
-[assembly: AssemblyProduct("WebGit .NET")]
-[assembly: AssemblyCopyright("Copyright © John Gietzen 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
 [assembly: Guid("c7b5234b-4e35-4791-b359-c59152302fb8")]
-
-[assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
