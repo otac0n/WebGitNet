56d99d1e25b489c82abc6a331f5915989e00f014
John Gietzen <otac0n@gietzen.us>
2012-03-28 11:07:00 -0700

    Added graph controller and view.

---
+++ WebGitNet.Graph/Controllers/GraphController.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GraphController.cs" company="(none)">
+//  Copyright © 2012 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+//-----------------------------------------------------------------------
+
+namespace WebGitNet.Controllers
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class GraphController : SharedControllerBase
+    {
+        public ActionResult ViewGraph(string repo)
+        {
+            return View();
+        }
+
+        public class RouteRegisterer : IRouteRegisterer
+        {
+            public void RegisterRoutes(RouteCollection routes)
+            {
+                routes.MapRoute(
+                    "View Graph",
+                    "browse/{repo}/graph",
+                    new { controller = "Graph", action = "ViewGraph", routeName = "View Graph" });
+
+                routes.MapResource("Scripts/repo-impact.js", "text/javascript");
+                routes.MapResource("Scripts/g.raphael/g.impact.js", "text/javascript");
+            }
+        }
+    }
+}
