07995db34e390eabd63a5457a81e28bc24d95fbb
John Gietzen <otac0n@gietzen.us>
2012-04-14 23:07:30 -0700

    Added the default search providers project.

---
+++ WebGitNet.SearchProviders/AssemblyInstaller.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInstaller.cs" company="(none)">
+//  Copyright © 2012 John Gietzen. All rights reserved.
+// </copyright>
+// <author>John Gietzen</author>
+//-----------------------------------------------------------------------
+
+namespace WebGitNet.SearchProviders
+{
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+    using WebGitNet.Search;
+
+    public class AssemblyInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(AllTypes.FromThisAssembly()
+                                       .BasedOn<ISearchProvider>()
+                                       .WithService.FromInterface());
+        }
+    }
+}
