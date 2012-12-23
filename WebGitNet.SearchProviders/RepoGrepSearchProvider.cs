487c8d201bda6c96fec88ae8caa7f29ea86400d6
John Gietzen <otac0n@gietzen.us>
2012-12-23 14:31:08 -0800

    Style.

--- WebGitNet.SearchProviders/RepoGrepSearchProvider.cs
+++ WebGitNet.SearchProviders/RepoGrepSearchProvider.cs
@@ -14,14 +14,17 @@
             {
                 return Task.Factory.StartNew(() => (IList<SearchResult>)Search(query, repository).Skip(skip).Take(count).ToList());
             }
-
-            return Task.Factory.StartNew(() => (IList<SearchResult>)Search(query, fileManager).Skip(skip).Take(count).ToList());
+            else
+            {
+                return Task.Factory.StartNew(() => (IList<SearchResult>)Search(query, fileManager).Skip(skip).Take(count).ToList());
+            }
         }
 
         private IEnumerable<SearchResult> Search(SearchQuery query, RepoInfo repo, bool includeRepoName = false)
         {
             var allTerms = string.Join(" --or ", query.Terms.Select(t => "-e " + GitUtilities.Q(t)));
             var commandResult = GitUtilities.Execute("grep --line-number --fixed-strings --ignore-case --context 3 --null --all-match " + allTerms + " HEAD", repo.RepoPath);
+
             var repoResults = commandResult.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
 
             return from m in repoResults
