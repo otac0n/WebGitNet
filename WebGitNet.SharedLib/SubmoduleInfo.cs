4f0a6f23437de75dcf16b1069b64be91d3d91ba0
David Lechner <david@lechnology.com>
2013-01-06 00:37:57 -0600

    changed submodule links from repo browse to link to the url specified in .submodules

---
+++ WebGitNet.SharedLib/SubmoduleInfo.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SubmoduleInfo.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace WebGitNet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class SubmoduleInfo
+    {        
+        public string Path
+        {
+          get;
+          private set;
+        }
+
+        public string Url
+        {
+          get;
+          private set;
+        }
+
+        public SubmoduleInfo(string path, string url)
+        {
+            this.Path = path;
+            this.Url = url;
+        }
+    }
+}
